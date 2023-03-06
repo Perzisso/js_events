892d93bbd651a117dbe0250c13b09637f72f3797
Florian <florian.sasso@outlook.fr>
2023-03-06 14:46:26 +0100

    Fonction 1

---
+++ script.js
@@ -0,0 +1,41 @@
+/*** Fonctionnalité 1 ***/
+
+var footer = document.querySelector('footer');
+
+var onClick = function() {
+  console.log('click');
+}
+
+footer.addEventListener("click", onClick);
+
+
+/*** Fonctionnalité 2 ***/
+
+
+
+/*** Fonctionnalité 3 ***/
+
+
+
+/*** Fonctionnalité 4 ***/
+
+
+
+/*** Fonctionnalité 5 ***/
+
+
+
+/*** Fonctionnalité 6 ***/
+
+
+
+/*** Fonctionnalité 7 ***/
+
+
+
+/*** Fonctionnalité 8 ***/
+
+
+
+/*** Fonctionnalité 0 ***/
+
