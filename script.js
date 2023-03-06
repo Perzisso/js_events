ac7c8dcde0899b1e34f9c76f146c43614fc763b7
Florian <florian.sasso@outlook.fr>
2023-03-06 15:34:21 +0100

    Fonction 2 and 3

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
 /*** Fonctionnalité 1 ***/
 
-var footer = document.querySelector('footer');
 
-var onClick = function() {
-  console.log('click');
+var footer = document.querySelector('footer');
+let c = 0
+
+var onClick1 = function() {
+  console.log('clique')
 }
 
-footer.addEventListener("click", onClick);
+var onClick2 = function() {
+  console.log(`clic numéro ${++c}`);
+  
+}
+footer.addEventListener("click", onClick1);
+footer.addEventListener("click", onClick2);
+footer.removeEventListener("click", onClick1);
+
 
 
 /*** Fonctionnalité 2 ***/
 
+var hamburger = document.querySelector(".navbar-toggler")
 
+var onClick = function() {
+    let to_toggle = document.getElementById("navbarHeader");
+    to_toggle.classList.toggle("collapse");
+};
+
+hamburger.addEventListener("click", onClick)
 
 /*** Fonctionnalité 3 ***/
 
